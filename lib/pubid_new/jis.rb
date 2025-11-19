0c1072d6835aca37b575dd7d51e7e5756df113d7
Ronald Tse <ronald.tse@ribose.com>
2025-11-19 20:29:50 +0800

    JIS

--- lib/pubid_new/jis.rb
+++ lib/pubid_new/jis.rb
@@ -1,16 +1,13 @@
+# frozen_string_literal: true
+
+require "lutaml/model"
 require_relative "jis/identifier"
-require_relative "jis/single_identifier"
-require_relative "jis/scheme"
-require_relative "jis/parser"
-require_relative "jis/builder"
 
 module PubidNew
   module Jis
-    # Main entry point for JIS identifiers
-    
-    def self.parse(identifier_string)
-      parsed = Parser.new.parse(identifier_string)
-      Builder.new(Scheme).build(parsed)
+    # Parse a JIS identifier string
+    def self.parse(identifier)
+      Identifier.parse(identifier)
     end
   end
 end
