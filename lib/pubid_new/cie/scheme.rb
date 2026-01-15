11edab0cd6f36d63c33bd86321d31e9b9ce6ce74
Ronald Tse <ronald.tse@ribose.com>
2026-01-15 23:53:43 +0800

    refactor(cie): use class method pattern for consistency with other flavors
    - Remove inheritance from PubidNew::Scheme
    - Change to class << self pattern
    - Convert @identifiers from instance variable to class method
    - Add supplement_identifiers method for corrigendum
    - Add typed_stages and supplement_typed_stages methods
    - Add locate_typed_stage_by_abbr and locate_identifier_klass_by_type_code methods
    - Update test to use class methods instead of instance methods

--- lib/pubid_new/cie/scheme.rb
+++ lib/pubid_new/cie/scheme.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "../scheme"
 require_relative "identifiers/standard"
 require_relative "identifiers/conference"
 require_relative "identifiers/corrigendum"
@@ -13,19 +12,62 @@ require_relative "identifiers/tutorial_bundle"
 
 module PubidNew
   module Cie
-    class Scheme < PubidNew::Scheme
-      def initialize
-        @identifiers = [
-          Identifiers::Standard,
-          Identifiers::Conference,
-          Identifiers::Corrigendum,
-          Identifiers::Bundle,
-          Identifiers::DualPublished,
-          Identifiers::Identical,
-          Identifiers::JointPublished,
-          Identifiers::Supplement,
-          Identifiers::TutorialBundle,
-        ].freeze
+    class Scheme
+      class << self
+        def identifiers
+          [
+            Identifiers::Standard,
+            Identifiers::Conference,
+            Identifiers::Bundle,
+            Identifiers::DualPublished,
+            Identifiers::Identical,
+            Identifiers::JointPublished,
+            Identifiers::Supplement,
+            Identifiers::TutorialBundle,
+          ]
+        end
+
+        def supplement_identifiers
+          [
+            Identifiers::Corrigendum,
+          ]
+        end
+
+        def typed_stages
+          identifiers.flat_map { |klass| klass::TYPED_STAGES }
+        end
+
+        def supplement_typed_stages
+          supplement_identifiers.flat_map { |klass| klass::TYPED_STAGES }
+        end
+
+        def locate_typed_stage_by_abbr(abbr)
+          abbr = "" if abbr.nil?
+
+          typed_stage = (typed_stages + supplement_typed_stages).detect do |ts|
+            ts.abbr.include?(abbr)
+          end
+
+          unless typed_stage
+            raise ArgumentError,
+                  "Unknown type abbreviation: '#{abbr}'"
+          end
+
+          typed_stage
+        end
+
+        def locate_identifier_klass_by_type_code(type_code)
+          identifier_klass = (identifiers + supplement_identifiers).detect do |klass|
+            klass.type[:key].to_s == type_code.to_s
+          end
+
+          unless identifier_klass
+            raise ArgumentError,
+                  "Unknown type code: #{type_code}"
+          end
+
+          identifier_klass
+        end
       end
     end
   end
--- spec/pubid_new/cie/scheme_spec.rb
+++ spec/pubid_new/cie/scheme_spec.rb
@@ -4,19 +4,24 @@ require "spec_helper"
 require_relative "../../../lib/pubid_new/cie/scheme"
 
 RSpec.describe PubidNew::Cie::Scheme do
-  describe "#identifiers" do
+  describe ".identifiers" do
     it "returns array of registered identifier classes" do
-      scheme = PubidNew::Cie::Scheme.new
-      expect(scheme.identifiers).to be_an(Array)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::Standard)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::Conference)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::Corrigendum)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::Bundle)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::DualPublished)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::Identical)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::JointPublished)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::Supplement)
-      expect(scheme.identifiers).to include(PubidNew::Cie::Identifiers::TutorialBundle)
+      expect(PubidNew::Cie::Scheme.identifiers).to be_an(Array)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::Standard)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::Conference)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::Bundle)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::DualPublished)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::Identical)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::JointPublished)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::Supplement)
+      expect(PubidNew::Cie::Scheme.identifiers).to include(PubidNew::Cie::Identifiers::TutorialBundle)
+    end
+  end
+
+  describe ".supplement_identifiers" do
+    it "returns array of registered supplement identifier classes" do
+      expect(PubidNew::Cie::Scheme.supplement_identifiers).to be_an(Array)
+      expect(PubidNew::Cie::Scheme.supplement_identifiers).to include(PubidNew::Cie::Identifiers::Corrigendum)
     end
   end
 end
