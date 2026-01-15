c50c5a34cb98f1dd170608efbe21548466988e34
Ronald Tse <ronald.tse@ribose.com>
2026-01-16 00:50:04 +0800

    test(asme): add error handling tests for Scheme

--- spec/pubid_new/asme/scheme_spec.rb
+++ spec/pubid_new/asme/scheme_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe PubidNew::Asme::Scheme do
       expect(PubidNew::Asme::Scheme.identifiers).to include(PubidNew::Asme::Identifiers::Standard)
     end
   end
+
+  describe ".locate_typed_stage_by_abbr" do
+    it "raises error indicating ASME does not use typed stages" do
+      expect { described_class.locate_typed_stage_by_abbr("anything") }
+        .to raise_error(ArgumentError, /ASME identifiers do not use typed stages/)
+    end
+  end
+
+  describe ".locate_identifier_klass_by_type_code" do
+    it "raises error indicating ASME does not use type codes" do
+      expect { described_class.locate_identifier_klass_by_type_code("anything") }
+        .to raise_error(ArgumentError, /ASME identifiers do not use type codes/)
+    end
+  end
 end
