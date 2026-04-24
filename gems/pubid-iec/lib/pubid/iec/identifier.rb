61aa5db3970295940dabb152a49136b7e773b280
Andrei Kislichenko <android.2net@gmail.com>
2026-04-23 23:52:31 -0400

    Add support for IEV shorthand parsing and related tests fixes metanorma/pubid-ieee#125

--- gems/pubid-iec/lib/pubid/iec/identifier.rb
+++ gems/pubid-iec/lib/pubid/iec/identifier.rb
@@ -1,10 +1,22 @@
 module Pubid::Iec
   module Identifier
+    IEV_SHORTHAND = /\AIEV(?=\z|[\s\-])/.freeze
+
     class << self
       include Pubid::Core::Identifier
 
       def parse(*args)
+        if args[0].is_a?(String) && args[0].match?(IEV_SHORTHAND)
+          args = args.dup
+          args[0] = args[0].sub(IEV_SHORTHAND, "IEC 60050")
+        end
         Base.parse(*args)
+      end
+
+      def parseable?(pubid)
+        return true if pubid.is_a?(String) && pubid.match?(IEV_SHORTHAND)
+
+        super
       end
 
       def build_project_stage(**args)
