f38f96bc3dad8392f195cc7055e1828c15444d42
Andrei Kislichenko <android.2net@gmail.com>
2026-07-14 15:30:29 +0000

    test(export): bump flavor count to 25 after ogc+w3c merge
    Merging main (which added the OGC flavor, 24) into this W3C branch brings
    Export::Exporter::FLAVORS to 25 entries (…sae ogc w3c). The count assertions
    in exporter_spec were left at 24 by the merge; update them to 25.

--- spec/pubid/export/exporter_spec.rb
+++ spec/pubid/export/exporter_spec.rb
@@ -5,8 +5,8 @@ require "pubid/export"
 
 RSpec.describe Pubid::Export::Exporter do
   describe "FLAVORS" do
-    it "lists all 24 flavors" do
-      expect(described_class::FLAVORS.size).to eq(24)
+    it "lists all 25 flavors" do
+      expect(described_class::FLAVORS.size).to eq(25)
     end
 
     it "includes iso" do
@@ -37,8 +37,8 @@ RSpec.describe Pubid::Export::Exporter do
       expect(data.keys).to all(be_a(String))
     end
 
-    it "exports all 24 flavors" do
-      expect(data.size).to eq(24)
+    it "exports all 25 flavors" do
+      expect(data.size).to eq(25)
     end
 
     it "exports ISO with identifier types" do
